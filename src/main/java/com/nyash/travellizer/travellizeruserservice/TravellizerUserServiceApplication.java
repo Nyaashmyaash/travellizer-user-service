9ba5c5ae8001d73101ddcc68631377cacdcb20ec
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-07 00:59:26 +0700

    Initial commit

---
+++ src/main/java/com/nyash/travellizer/travellizeruserservice/TravellizerUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.travellizer.travellizeruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellizerUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravellizerUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyash/travellizer/travellizeruserservice/TravellizerUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.travellizer.travellizeruserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellizerUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
